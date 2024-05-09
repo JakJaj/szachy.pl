90ae1fae4193355faf4f993e403c5d5ca443118f
Jakub Jajonek <jakub.jajonek@gmail.com>
2024-05-09 23:33:32 +0200

    add empty menu page

---
+++ lib/menu.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+class Menu extends StatelessWidget {
+  const Menu({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Basic Page'),
+      ),
+      body: Center(
+        child: Text(
+          'Hello, World!',
+          style: TextStyle(fontSize: 24),
+        ),
+      ),
+    );
+  }
+}
